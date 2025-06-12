5d0a2659e411be67d8becb02037fd87bf7f3be1d
debarchita-123 <debarchita2020@gmail.com>
2025-06-12 23:48:32 +0530

    Implemented Auth, Billing, Database Setup, Create Companion Server action

---
+++ app/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import { SignUp } from "@clerk/nextjs";
+
+const Page = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <SignUp />
+    </div>
+  )
+}
+
+export default Page
